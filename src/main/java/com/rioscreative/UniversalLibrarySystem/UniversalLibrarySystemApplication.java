47dd41fc151a70acf22c5cd1f144c4a7c914c0ef
Luis Rios <luis.rios@fdmgroup.com>
2020-11-04 15:07:50 -0500

    Initial SpringBoot project setup

---
+++ src/main/java/com/rioscreative/UniversalLibrarySystem/UniversalLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.rioscreative.UniversalLibrarySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rioscreative/UniversalLibrarySystem/UniversalLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rioscreative.UniversalLibrarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
